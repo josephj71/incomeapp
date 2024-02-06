bdfa061b6928ac746a0d8210077110592b9f928e
deeprogrammer <joseph_johnson_71@yahoo.com>
2024-02-06 00:41:25 -0600

    First rust project

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::{primitive, string};
+
+use slint::{SharedString, Weak};
+
+slint::include_modules!();
+
+const TAXPER: f64 = 0.30;
+const OWNERPER: f64 = 0.55;
+const PROFITPER: f64 = 0.05;
+const OPEXPER: f64 = 0.10;
+
+fn main() -> Result<(), slint::PlatformError> {
+    let ui = AppWindow::new()?;
+
+    ui.on_divide_income({
+        let ui_handle = ui.as_weak();
+        move |string: SharedString| {
+            let ui = ui_handle.unwrap();
+            let num: f64 = string.trim().parse().unwrap();
+            let tax: f64 = num * TAXPER;
+            let owner:f64 = num * OWNERPER;
+            let profit:f64 = num * PROFITPER;
+            let opex:f64 = num * OPEXPER;
+            let result: String = format!("Taxes: {:2}\nOwner: {:2}\nProfit: {:2}\nOpex: {:2}", {tax}, {owner}, {profit}, {opex});
+            ui.set_results(result.into())
+        }
+    });
+
+    // ui.on_request_increase_value({
+    //     let ui_handle = ui.as_weak();
+    //     move || {
+    //         let ui = ui_handle.unwrap();
+    //         ui.set_counter(ui.get_counter() + 1);
+    //     }
+    // });
+
+    ui.run()
+}
